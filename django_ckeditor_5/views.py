1f74a5f20944027354dc6943c8fc5b60a5095c5f
Jan Lajara <jan@processshepherd.com>
2025-05-01 08:06:20 +0800

    upgrade to use django 5.0

--- django_ckeditor_5/views.py
+++ django_ckeditor_5/views.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import urllib.parse
 from django.http import Http404
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.http import JsonResponse
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
--- django_ckeditor_5/widgets.py
+++ django_ckeditor_5/widgets.py
@@ -2,7 +2,7 @@ from django import forms
 from django.conf import settings
 from django.forms.renderers import get_default_renderer
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.forms.utils import ErrorList
 
 DEFAULT_CONFIG = {
